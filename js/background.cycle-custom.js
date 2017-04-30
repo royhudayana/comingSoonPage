9cb0aaa2eeaa48f1d7ef18740100499e645f4f54
royhudayana <royanhudayana@gmail.com>
2017-04-30 12:57:55 +0700

    Versi 1.0.0

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var fadeShow = $(".background").fadeShow({
+
+	correctRatio: true,
+
+	shuffle: true,
+
+	speed: 1,
+
+	images: ['images/bg-image-2.jpg',
+
+			 'images/bg-image-3.jpg']
+
+
+
+});
